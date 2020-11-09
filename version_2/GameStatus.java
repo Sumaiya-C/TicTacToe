364312f90cc1a32f3bbce52f748464e2633373b9
Sumaiya-C <sumaiya.chowdhury17@northsouth.edu>
2020-11-10 01:49:27 +0600

    Updated names,added a folder and a text file

--- version_2/GameStatus.java
+++ version_2/GameStatus.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author Andrew Vardy
+ * @author Sumaiya Chowdhury
  */
 
 public enum GameStatus {
--- version_2/Move.java
+++ version_2/Move.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author Andrew Vardy
+ * @author Sumaiya Chowdhury
  */
 
 public class Move {
